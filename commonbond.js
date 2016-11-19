3ddeb0cd3c1f643b1a2476f2e70b368c299562dd
dataface <monson.exe@gmail.com>
2016-11-19 14:41:30 -0500

    make an 'x' whenever left and right forces collide

--- commonbond.js
+++ commonbond.js
@@ -1,3 +1,7 @@
+// TODO
+// refactor with new sorting algorithm
+// introduce speed setting
+// figure out why an object format would be good for this
 
 function getIndices (arr, val) {
 	var indices = [],
@@ -12,7 +16,7 @@ function getIndices (arr, val) {
 	return indices;
 }
 
-var thebombs = '.B......B.B...';
+var thebombs = '...B....BB.B.';
 
 function detonate (bombs) {
 
@@ -25,6 +29,8 @@ function detonate (bombs) {
 
 	// step 1
 	for (b in locations) {
+		// I could use a better sorting algorithm to alternate between l and right
+		// in a frame FULL of bombs we could explode them at the same time (?)
 		var bomb = locations[b];
 
 		frame[bomb] = '.';
@@ -38,19 +44,29 @@ function detonate (bombs) {
 	tick(frame);
 
 	function tick (frame) {
-		// consolidate these two into 1 array and then if-statement them later
+		// consolidate these two into 1 array and then if-statement them later (?)
 		rightForce = getIndices(frame, '>'),
-		leftForce = getIndices(frame, '<');
+		leftForce = getIndices(frame, '<'),
+		crossfire = getIndices(frame, 'x');
 
-		if (!rightForce.length && !leftForce.length) {
+		// stop if there's nothing left
+		if (!rightForce.length && !leftForce.length && !crossfire.length) {
 			return;
+		}
+
+		for (x in crossfire) {
+			frame[crossfire[x]] = '.';
 		}
 
 		for (l in leftForce) {
 			frame[leftForce[l]] = '.';
 
 			if (leftForce[l] - 1 <= frame.length) {
-				frame[leftForce[l] - 1] = '<';
+				if (frame[leftForce[l] - 1] === '>') {
+					frame[leftForce[l] - 1] = 'x';
+				} else {
+					frame[leftForce[l] - 1] = '<';
+				}
 			}
 		}
 
@@ -58,7 +74,11 @@ function detonate (bombs) {
 			frame[rightForce[r]] = '.';
 
 			if (rightForce[r] + 1 < frame.length) {
-				frame[rightForce[r] + 1] = '>';
+				if (frame[rightForce[r] + 1] === '<') {
+					frame[rightForce[r] + 1] = 'x';
+				} else {
+					frame[rightForce[r] + 1] = '>';
+				}
 			}
 		}
 
