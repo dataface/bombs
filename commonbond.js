dbd7d4d1ca5c2171de394786a643b1497a9c4d4a
dataface <monson.exe@gmail.com>
2016-11-30 21:32:01 -0500

    add options to script

--- commonbond.js
+++ commonbond.js
@@ -1,7 +1,17 @@
 // TODO
-// refactor with new sorting algorithm
 // introduce speed setting
+// replace bomb with exploded version at first step
 // figure out why an object format would be good for this
+
+
+var c = {
+	bombscape: '____o_____o_oo_____o____',
+	bombs: 'o',
+	nothing: '_',
+	lforce: '<',
+	rforce: '>',
+	cfire: '*'
+}
 
 // --- a little helper
 function getIndices (arr, val) {
@@ -17,12 +27,10 @@ function getIndices (arr, val) {
 	return indices;
 }
 
-var thebombs = '......B...B......';
+function detonate (config) {
 
-function detonate (bombs) {
-
-	var frame = bombs.split(''),
-		locations = getIndices(frame, 'B'),
+	var frame = config.bombscape.split(''),
+		locations = getIndices(frame, config.bombs),
 		rightForce,
 		leftForce;
 
@@ -31,10 +39,9 @@ function detonate (bombs) {
 		// in a frame FULL of bombs we could explode them at the same time (?)
 		var bomb = locations[b];
 
-		frame[bomb] = '.';
-
-		tickLeft(bomb, frame);
+		frame[bomb] = config.nothing;
 		tickRight(bomb, frame);
+		tickLeft(bomb, frame);
 	}
 
 	console.log(frame.join(''));
@@ -42,28 +49,32 @@ function detonate (bombs) {
 	tick(frame);
 
 	function tick (frame) {
-		// consolidate these two into 1 array and then if-statement them later (?)
-		rightForce = getIndices(frame, '>'), // these should start as 'B' and THEN become an arrow
-		leftForce = getIndices(frame, '<'),
-		forces = rightForce.concat(leftForce), // maybe a bad idea
-		crossfire = getIndices(frame, 'x');
+		rightForce = getIndices(frame, config.rforce),
+		leftForce = getIndices(frame, config.lforce),
+		forces = rightForce.concat(leftForce), // THIS IS TEMPORARY
+		crossfire = getIndices(frame, config.cfire);
 
+		// if left and right forces collide we form crossfire
 		for (x in crossfire) {
-			frame[crossfire[x]] = '.';
+			frame[crossfire[x]] = config.nothing;
 		}
 
 		for (f in forces) {
 
-			if (frame[forces[f]] === '>') {
+			// force right
+			if (frame[forces[f]] === config.rforce) {
 				tickRight(forces[f], frame);
 
-			} else if (frame[forces[f]] === '<') {
+			// force left
+			} else if (frame[forces[f]] === config.lforce) {
 				tickLeft(forces[f], frame);
 			}
 
-			frame[forces[f]] = '.';
+			// clear the previous force
+			frame[forces[f]] = config.nothing;
 		}
 
+		// this is crappy and i need a better way to return the value
 		console.log(frame.join(''));
 
 		if (forces.length || crossfire.length) {
@@ -74,27 +85,28 @@ function detonate (bombs) {
 	function tickRight (index, arr) {
 
 		if (index + 1 < arr.length) {
-			if (arr[index + 1] === '<') {
-				arr[index + 1] = 'x';
+			if (arr[index + 1] === config.lforce) {
+				arr[index + 1] = config.cfire;
 			} else {
-				arr[index + 1] = '>';
+				arr[index + 1] = config.rforce;
 			}
 		}
 	}
 
 	function tickLeft (index, arr) {
 		if (index - 1 >= 0) {
-			if (arr[index - 1] === '>') {
-				arr[index - 1] = 'x';
+			if (arr[index - 1] === config.rforce) {
+				arr[index - 1] = config.cfire;
 			} else {
-				arr[index - 1] = '<';
+				arr[index - 1] = config.lforce;
 			}
 		}
 	}
 }
 
-console.log(thebombs);
+// demonstrate what bombs we're starting with (stupid)
+console.log(c.bombscape);
 
-detonate(thebombs);
+detonate(c);
 
 
